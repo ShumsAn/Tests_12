3645261fd467c153eb2176994daac7c84985ee7e
ShumsAn <shum-sol@outlook.com>
2024-10-31 17:48:30 +0300

    "Методы Юнит-тестирования"
    Цель: освоить методы, которые содержит класс TestCase.

--- tests_12_2.py
+++ tests_12_2.py
@@ -2,27 +2,27 @@ import runner_and_tournament as RT
 import unittest
 
 class TournamentTest(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
 
+    @classmethod
+    def tearDownClass(cls):
+        for place,name in cls.all_results.items():
+            print(place,name)
 
     def setUp(self):
         self.Bolt = RT.Runner('Усэйн',10)
         self.Andrey = RT.Runner('Андрей',9)
         self.Nik = RT.Runner('Ник',3)
 
-    @classmethod
-    def tearDownClass(cls):
-        print(cls.all_results)
 
     def test_run(self):
         Marathon = RT.Tournament(90, self.Bolt, self.Nik)
-        all_results = Marathon.start()
-        self.assertTrue(all_results[2] == self.Nik)
+        TournamentTest.all_results = Marathon.start()
+        self.assertTrue(TournamentTest.all_results[2] == self.Nik)
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
